0db55ec7ffb224233ec70f733ea56b35fbdf5260
Xiuer <Xiuer@example.com>
2022-09-06 11:19:22 +0800

    comment:项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const cpns = resolve(__dirname, 'src/components')
+const views = resolve(__dirname, 'src/views')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      host: 'localhost',
+      port: 8886
+    },
+    resolve: {
+      alias: {
+        cpns,
+        views
+      }
+    }
+  }
 })
